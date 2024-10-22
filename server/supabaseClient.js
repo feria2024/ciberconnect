3517691f3bd456d0adec14319b9c7dc9eb1d6a48
cjhrnndz <oscardelgadoh1@gmail.com>
2024-10-22 00:38:16 -0600

    Update supabaseClient.js

--- server/supabaseClient.js
+++ server/supabaseClient.js
@@ -1,5 +1,7 @@
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://pptivfzqjmetxjsjmvzz.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
